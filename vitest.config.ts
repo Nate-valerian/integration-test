0e1a1905904e61e70b4b265f36446e39d0efed73
Nate Valerian <n.nate1544@gmail.com>
2025-12-18 20:26:34 +0300

    first integration testing

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: "jsdom",
+      setupFiles: "./vitest.setup.ts",
+      globals: true,
+       include: [
+              "./tests/integration/**/*.test.{ts,tsx}",
+              "./tests/unit/**/*.test.{ts,tsx}",
+            ],
+      reporters: "verbose",
+      clearMocks: true,
+      watch: false,
+
+  },
+});
